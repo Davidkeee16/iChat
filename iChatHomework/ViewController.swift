e0e0744e3aa0317cc677506e348701358f5e916c
David <davidkeee16@gmail.com>
2025-06-16 16:33:43 +0300

    Initial Commit

---
+++ iChatHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChatHomework
+//
+//  Created by David Puksanskis on 16/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
